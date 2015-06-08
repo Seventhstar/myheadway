350c998fe9ca431b60cef01174b2f658d32f200e
seventhstar <seventhstar@mail.ru>
2015-06-08 14:23:51 +0300

    feelings date format

---
+++ db/migrate/20150608112020_change_start_date_denials.rb
@@ -0,0 +1,10 @@
+class ChangeStartDateDenials < ActiveRecord::Migration
+  def change
+    reversible do |dir|
+      change_table :denials do |t|
+        dir.up   { t.change :start_date, :date }
+        dir.down { t.change :start_date, :datetime }
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309172036) do
+ActiveRecord::Schema.define(version: 20150608112020) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name",       limit: 255
