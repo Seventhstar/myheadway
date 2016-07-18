98af65b356ca67c2dce5e29d23f843f8f5407796
seventhstar <seventhstar@mail.ru>
2016-07-18 08:08:59 +0300

    add tag

--- public/assets/application-0aaf47d5743fc462a1f347fac8014b85761c68b195a7cc1de8c61b5fa62a5c34.js
+++ public/assets/application-0aaf47d5743fc462a1f347fac8014b85761c68b195a7cc1de8c61b5fa62a5c34.js
@@ -34367,6 +34367,12 @@ $(document).ready(function(){
       display_selected_options: false,
       display_disabled_options: false
     });
+    $('.icon_code').click(function() {
+      return insertAtCaret('doc_description', '<source></source>', 8);
+    });
+    $('.icon_tag').click(function() {
+      return insertAtCaret('doc_description', '<span class="key"></span>', 18);
+    });
     $('.container').on('click', 'span.sw_check', function() {
       var checked, chk, item_id, model, table;
       model = $(this).parents('table').attr('model');
@@ -34586,6 +34592,40 @@ $(document).ready(function(){
   });
 
 }).call(this);
+function insertAtCaret(areaId,text,addPos = 0) {
+    var txtarea = document.getElementById(areaId);
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var strSet = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? "ff" : (document.selection ? "ie" : false ) );
+    if (br == "ie") { 
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        strPos = range.text.length;
+    }
+    else if (br == "ff") {strPos = txtarea.selectionStart; strSet = strPos+addPos;}
+
+    var front = (txtarea.value).substring(0,strPos);  
+    var back = (txtarea.value).substring(strPos,txtarea.value.length); 
+    txtarea.value = front+text+back;
+    strPos = strPos + text.length;
+    if (br == "ie") { 
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        range.moveStart ('character', strPos);
+        range.moveEnd ('character', 0);
+        range.select();
+    }
+    else if (br == "ff") {
+        txtarea.selectionStart = strSet;
+        txtarea.selectionEnd = strSet;
+        txtarea.focus();
+    }
+    txtarea.scrollTop = scrollPos;
+}
+;
 (function() {
 
 
