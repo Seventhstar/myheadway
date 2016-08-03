16b5b16be4847184d62f6b53eb1cb1ab6591df92
seventhstar <seventhstar@mail.ru>
2016-08-03 21:34:07 +0300

    noimage

--- db/schema.rb
+++ db/schema.rb
@@ -33,13 +33,13 @@ ActiveRecord::Schema.define(version: 20160802062626) do
   end
 
   create_table "authors", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "books", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -58,10 +58,10 @@ ActiveRecord::Schema.define(version: 20160802062626) do
   end
 
   create_table "denials", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.date     "start_date"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "start_date"
   end
 
   create_table "doc_types", force: :cascade do |t|
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 20160802062626) do
   end
 
   create_table "search_suggestions", force: :cascade do |t|
-    t.string   "term",       limit: 255
+    t.string   "term"
     t.integer  "author"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -102,12 +102,12 @@ ActiveRecord::Schema.define(version: 20160802062626) do
 
   create_table "statements", force: :cascade do |t|
     t.integer  "author_id"
-    t.string   "source",     limit: 255
-    t.string   "theme",      limit: 255
+    t.string   "source"
+    t.string   "theme"
     t.text     "content"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.integer  "book_id"
     t.boolean  "isTip"
   end
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 20160802062626) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -145,7 +145,7 @@ ActiveRecord::Schema.define(version: 20160802062626) do
   end
 
   create_table "targets", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "parent_id"
     t.integer  "user_id"
     t.integer  "group_id"
